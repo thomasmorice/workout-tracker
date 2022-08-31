000ea320295e5ce11a6edda25b41eec4d5974070
Thomas Morice <t.morice4@gmail.com>
2022-08-31 11:13:58 +0200

    try fix drag & drop on mobile

--- src/components/WorkoutSession/WorkoutSessionForm/WorkoutSessionResultDraggable.tsx
+++ src/components/WorkoutSession/WorkoutSessionForm/WorkoutSessionResultDraggable.tsx
@@ -3,7 +3,7 @@ import { Reorder, useDragControls, useMotionValue } from "framer-motion";
 import { CreateWorkoutSessionInputSchema } from "../../../server/router/workout-session";
 import { DifficultyBadge } from "../../Workout/WorkoutBadges";
 import { MdRemove, MdDragIndicator } from "react-icons/md";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 type CreateWorkoutSessionInput = z.infer<
   typeof CreateWorkoutSessionInputSchema
@@ -22,7 +22,16 @@ export default function WorkoutSessionResultDraggable({
 }: WorkoutSessionResultDraggableProps) {
   const y = useMotionValue(0);
   const dragControls = useDragControls();
-  const [isDragged, set_isDragged] = useState(false);
+  const [isDragging, set_isDragging] = useState(false);
+
+  useEffect(() => {
+    if (isDragging) {
+      document.body.classList.add("select-none", "touch-none");
+    } else {
+      document.body.classList.remove("select-none", "touch-none");
+    }
+  }, [isDragging]);
+
   return (
     <Reorder.Item
       dragListener={false}
@@ -34,27 +43,23 @@ export default function WorkoutSessionResultDraggable({
         position: "relative",
         zIndex: 999,
       }}
-      onMouseDown={() => {
-        document.body.classList.add("select-none");
-        set_isDragged(true);
-      }}
-      onMouseUp={() => {
-        document.body.classList.add("select-none");
-        set_isDragged(false);
-      }}
+      onMouseDown={() => set_isDragging(true)}
+      onMouseUp={() => set_isDragging(false)}
+      onTouchStart={() => set_isDragging(true)}
+      onTouchEnd={() => set_isDragging(false)}
       className="mb-5"
     >
       <div
         className={`flex flex-col px-5 py-4 bg-base-200 gap-1 rounded-lg transition-all ${
-          isDragged && "shadow-xl bg-base-300"
+          isDragging && "shadow-xl bg-base-300"
         }`}
       >
         <MdDragIndicator
           onPointerDown={(e) => dragControls.start(e)}
-          className="absolute right-2 w-7 h-7 cursor-pointer"
+          className="absolute right-3 w-7 h-7 cursor-pointer"
         />
         <div className=" flex items-center gap-2 ">
-          <div className="flex items-center ">
+          {/* <div className="flex items-center ">
             <button
               type="button"
               onClick={() => onRemoveWorkoutResult(result)}
@@ -62,26 +67,36 @@ export default function WorkoutSessionResultDraggable({
             >
               <MdRemove size={27} className="text-error p-1" />
             </button>
-          </div>
+          </div> */}
           <div className="text-xl">
             {result.workout.name || `#${result.workout.id}`}
           </div>
           <DifficultyBadge difficulty={result.workout.difficulty} />
         </div>
-        <div className="overflow-hidden text-ellipsis whitespace-pre-wrap cursor-default text-2xs">
+        <div className="overflow-hidden text-ellipsis whitespace-pre-wrap cursor-default text-2xs mt-4">
           {result.workout.description}
         </div>
-        <button
-          type="button"
-          onClick={() => {
-            onOpenWorkoutResultDetail(result);
-          }}
-          className="mt-4 btn btn-primary btn-sm"
-        >
-          <div className="">{`${
-            result.workout.description ? "edit" : "save"
-          } result`}</div>
-        </button>
+        <div className="flex mt-4  gap-2 items-center">
+          <button
+            type="button"
+            onClick={() => {
+              onOpenWorkoutResultDetail(result);
+            }}
+            className="btn btn-primary btn-sm w-1/2"
+          >
+            <div className="">{`${
+              result.workout.description ? "edit" : "save"
+            } result`}</div>
+          </button>
+
+          <button
+            type="button"
+            onClick={() => onRemoveWorkoutResult(result)}
+            className="btn btn-sm btn-error w-1/2"
+          >
+            Remove workout
+          </button>
+        </div>
       </div>
     </Reorder.Item>
   );
