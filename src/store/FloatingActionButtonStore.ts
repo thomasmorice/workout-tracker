eae03f738ce779066b86be304fcc0b9090a9b1d2
Thomas Morice <t.morice4@gmail.com>
2023-01-15 13:12:50 +0100

    undo changes

--- src/store/FloatingActionButtonStore.ts
+++ src/store/FloatingActionButtonStore.ts
@@ -31,7 +31,7 @@ const useFloatingActionButtonStore = create<FloatingActionButtonState>()(
           selectedWorkouts: selectedWorkouts.filter((w) => w.id !== workout.id),
         });
       } else {
-        navigator.vibrate(200);
+        navigator.vibrate(50);
         set({
           selectedWorkouts: [...selectedWorkouts, workout],
         });
