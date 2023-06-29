046ceaf644751d5f4b8d30bd3b8f946a0865730f
Thomas Morice <t.morice4@gmail.com>
2023-06-29 23:38:11 +0200

    css must be global

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import { SessionProvider } from "next-auth/react";
 import { trpc } from "../utils/trpc";
 import "../styles/globals.css";
 import Layout from "../components/Layout/Layout";
-import "./multiRangeSlider.css";
+import "../components/MultiRangeSlider/multiRangeSlider.css";
 // import { Roboto } from "@next/font/google";
 
 const MyApp: AppType<{ session: Session | null }> = ({
